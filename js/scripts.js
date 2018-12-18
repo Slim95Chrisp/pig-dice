18bb36d608a5f7dfd7204d7e80979a7ffdc0b7c3
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-18 13:45:34 +0300

    add user interface script

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+var scores, roundScore, activePlayer;
+
+inti();
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. random num
+    var dice = Math.floor(Math.random() *6 ) + 1;
+
+    //2. display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+ dice +'.png';
+
+
+    //3. update the round if the rolled number was not a 1
+    if(dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    }
+    else {
+        //next player
+        nextPlayer();
+        }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //1. add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //2. update the UI
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer]
+
+    //3. check if player wone the game
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name-'+activePlayer).textContent = "Winner";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer + '-panel').classList.add('Winner');
+        document.querySelector('.player-'+activePlayer + '-panel').classList.remove('Winner');
+    }
+    else {
+      nextPlayer();
+    }
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', inti);
