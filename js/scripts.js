c05ce2a838a0587b274e256347b71d603d8a3d4a
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-18 13:46:49 +0300

    add business logic to js

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,26 @@ function nextPlayer(){
 }
 
 document.querySelector('.btn-new').addEventListener('click', inti);
+
+function inti(){
+    scores = [0,0];
+    activePlayer = 0 ;
+    roundScore  = 0;
+
+    document.querySelector('.dice').style.display = 'block';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = "Player 1";
+    document.getElementById('name-1').textContent = "Player 2";
+    document.querySelector('.player-0-panel').classList.remove('Winner');
+    document.querySelector('.player-1-panel').classList.remove('Winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+
+
+}
