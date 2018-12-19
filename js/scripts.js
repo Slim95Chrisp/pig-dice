b4e6c07cd389fbd0fab6392d2451b11abbcf1dc4
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-19 03:37:24 +0300

    add new game function

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,24 @@ var myTurn1 = 0;
 var myTurn2 = 0;
 var player1Total = 0;
 var player2Total= 0;
+
+function rollNumber() {
+  playerRoll = roll()
+  if (playerRoll === 1) {
+    playerRoll = 0;
+    alert("Rolled 1 your score is 0, " + player + " turn.");
+  }
+};
+
+function newGame() {
+  playerRoll = 0;
+  total = 0;
+  player1Total = 0;
+  player2Total = 0;
+  myTurn1 = 0;
+  myTurn2 = 0;
+  $("#total1").val("");
+  $("#current1").val("");
+  $("#total2").val("");
+  $("#current2").val("");
+}
